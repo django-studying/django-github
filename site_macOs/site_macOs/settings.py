eb2195a4de9e00303ec7d78f75c068cb76336cbb
user-macOs <shirshov.kirill83@gmail.com>
2022-10-27 22:26:42 +0300

    learn at home

--- site_macOs/site_macOs/settings.py
+++ site_macOs/site_macOs/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'polls.apps.PollsConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
